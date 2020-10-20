fd37ddb6734fb5c6d7b5f987d097b04d5a484b36
Rylan <rschubkegel17@georgefox.edu>
2020-10-20 10:24:22 -0700

    Little more work on setting up ASCII board

--- pages/js/script.js
+++ pages/js/script.js
@@ -2,7 +2,8 @@
 
 const ALIVE = 'X';
 const DEAD = '.';
-const BOARD_SIZE = 32;
+const BOARD_TILES = 32;
+const BOARD_SIZE = "60vmin";
 const FOREGROUND_COLOR = "#7ae570";
 const BACKGROUND_COLOR = "#4a514b";
 
@@ -10,19 +11,45 @@ const BACKGROUND_COLOR = "#4a514b";
 function start() {
     console.log("start called");
 
+    // set up CSS
+    console.log("setting up CSS");
+    $("*").css({
+        "margin": "0",
+        "padding": "0",
+        "font-family": "monospace",
+        //"outline": "red solid 1px",
+    });
+    $("#page-wrapper").css({
+        "display": "flex",
+        "flex-direction": "row",
+        "align-items": "center",
+        "width": "100vw",
+        "height": "100vh",
+    });
+    $(".center-pane").css("flex", "1 1 " + BOARD_SIZE);
+    $(".side-pane").css("flex", "1 1 20%");
+
     // add board
     console.log("adding characters to board");
     let board = $("#board");
     let content = "";
-    for (let y = 0; y < BOARD_SIZE; y++)
+    for (let y = 0; y < BOARD_TILES; y++)
     {
-        for (let y = 0; y < BOARD_SIZE; y++)
+        for (let y = 0; y < BOARD_TILES; y++)
         {
             content += DEAD;
         }
         content += "\n";
     }
     board.html(content);
+
+    // resize characters
+    let fontSize = "calc(" + BOARD_SIZE + " / " + BOARD_TILES + ")";
+    board.css({
+        "font-size": fontSize,
+        "letter-spacing": "0.5em",
+        "text-align": "center",
+    });
 
     // set color
     console.log("setting color");
