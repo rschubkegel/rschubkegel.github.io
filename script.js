ed5744ed65eb9cf4c10a90a87d34ac5cb864ff2b
Rylan <rschubkegel17@georgefox.edu>
2020-10-30 12:49:40 -0700

    Added more animations to landing page

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 function start() {
     console.log("initializing webpage");
 
+    // fade in the portfolio banner
     anime({
         targets: "#portfolio",
         opacity: "20%",
-        duration: 20000,
+        duration: 5000,
+        left: "-4.2em",
         easing: "easeOutExpo"
+    });
+
+    // rotate icon
+    anime({
+        targets: "#logo",
+        rotate: "360deg",
+        easing: "spring(1, 50, 8, 0)"
+    });
+
+    // slide in headers
+    anime({
+        targets: "h1, h2, h3",
+        marginLeft: "0",
+        opacity: "100%",
+        duration: 1000,
+        easing: "easeOutCubic"
+    });
+
+    // expand horizontal rules
+    anime({
+        targets: "hr",
+        width: "100%",
+        opacity: "50%",
+        duration: 1000,
+        easing: "easeOutBounce"
     });
 }
