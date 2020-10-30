907f6469dd13170f1133ecb7241c8776fb7a8dfc
Rylan Schubkegel <rschubkegel17@georgefox.edu>
2020-10-30 09:51:46 -0700

    Start adding animations to landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+function start() {
+    console.log("initializing webpage");
+
+    anime({
+        targets: "#portfolio",
+        opacity: "20%",
+        duration: 20000,
+        easing: "easeOutExpo"
+    });
+}
