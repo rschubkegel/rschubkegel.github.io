c312a9331b1422b5698b825cbee2582acf20a10f
Rylan Schubkegel <rylanschubkegel@gmail.com>
2021-08-31 18:09:51 -0700

    Add TOC

--- script.js
+++ script.js
@@ -58,7 +58,11 @@ function playAnimations() {
 function addSectionLinks() {
     console.log("Adding links to h2 elements");
     var headers = document.getElementsByTagName("h2");
+
+    // add links for every h2 element
     for (i = 0; i < headers.length; i++) {
+
+        // add > symbol next to header
         console.log("Adding link to section " + headers[i].innerHTML);
         headers[i].innerHTML =
             '<a href="#'
@@ -66,4 +70,39 @@ function addSectionLinks() {
             + '" class="section-link">&gt;</a> '
             + headers[i].innerHTML;
     }
+}
+
+function generateTOC() {
+    console.log("Generating TOC");
+
+    // add TOC to DOM
+    var welcomeDiv = document.getElementById("section-welcome");
+    welcomeDiv.appendChild(document.createElement("hr"));
+    var tocHeader = document.createElement("h2");
+    tocHeader.innerHTML = "Table of Contents";
+    var toc = document.createElement("ul");
+
+    // get headers
+    var headers = document.getElementsByTagName("h2");
+
+    // add link for every h2 element on page
+    for (i = 0; i < headers.length; i++) {
+
+        // add section link to TOC
+        var nameIndex = headers[i].innerHTML.search("</a>") + 5;
+        var name = headers[i].innerHTML.substring(nameIndex);
+        console.log("Adding link for " + name);
+
+        var link = document.createElement("li");
+        link.innerHTML =
+            '<a href="#'
+            + headers[i].parentElement.id
+            + '">'
+            + name
+            + '</a>';
+        toc.appendChild(link);
+    }
+
+    welcomeDiv.appendChild(tocHeader);
+    welcomeDiv.appendChild(toc);
 }
