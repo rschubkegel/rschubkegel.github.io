67cf9dcb30309f2c6f35bb4a22e70d14098d1590
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-02-10 10:40:36 -0800

    Clean SVG, add logo animation option

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+  // move section links to end of header
   Array.from(document.querySelectorAll(".header-link")).forEach((link) => {
     let p = link.parentNode;
     p.previousElementSibling.appendChild(link);
