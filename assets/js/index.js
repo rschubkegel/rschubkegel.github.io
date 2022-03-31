34541d4ff421241b8b978166f447249fd06f68fc
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-03-30 17:04:48 -0700

    Add simple notification when header clicked

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,15 @@
  * Do stuff when document loads.
  */
 window.addEventListener("load", function () {
-  append_section_links_to_headers();
-  add_id_copy_listeners_to_headers();
+  appendSectionLinksToHeaders();
+  addCopyListenersToHeaders();
 });
 
 /**
  * Re-parents any .header-link nodes
  * under its current parent's previous sibling.
  */
-function append_section_links_to_headers() {
+function appendSectionLinksToHeaders() {
   Array.from(document.querySelectorAll(".header-link")).forEach((link) => {
     let p = link.parentNode;
     p.previousElementSibling.appendChild(link);
@@ -21,14 +21,41 @@ function append_section_links_to_headers() {
 /**
  * Copies a page link with #id of the header that's been clicked.
  */
-function add_id_copy_listeners_to_headers() {
-  let h_selectors = "h1, h2, h3, h4, h5, h6";
+function addCopyListenersToHeaders() {
+  let h_selectors = "h2, h3, h4, h5, h6";
   Array.from(document.querySelectorAll(h_selectors)).forEach((header) => {
     header.onclick = () => {
-      let t = `${window.location.href}#${header.id}`;
+      let t = `${window.location.href.replace(location.hash,"")}#${header.id}`;
       navigator.clipboard.writeText(t);
-      console.log(`Copied "${t}" to clipboard`);
-      // header.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      header.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+      showCopiedMessage(t);
     };
   });
+}
+
+/**
+ * Shows DOM element to notify user that URL was copied to clipboard.
+ */
+function showCopiedMessage(message) {
+  fade("copy-notification", 1, 0, 2000, new Date().getTime());
+}
+
+// https://blog.abhranil.net/2011/11/03/simplest-javascript-fade-animation/
+function fade(eid, initOp, finalOp, TimeToFade, time)
+{
+  document.getElementById(eid).style.visibility = "visible";
+  var curTick = new Date().getTime();
+  var elapsedTicks = curTick - time;
+  var newOp = initOp+(finalOp-initOp)*elapsedTicks/TimeToFade;
+  if (Math.abs(newOp-initOp)>Math.abs(finalOp-initOp))
+  {
+    document.getElementById(eid).style.opacity=finalOp;
+    if (finalOp==0)
+    {
+        document.getElementById(eid).style.visibility="hidden"
+    }
+    return;
+  }
+  document.getElementById(eid).style.opacity=newOp;
+  setTimeout("fade( '" + eid + "'," + initOp + "," + finalOp + "," + TimeToFade + "," + time + ")", TimeToFade/100);
 }
