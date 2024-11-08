77636d787fc2276ef403adae50192f41a1650d64
Rylan Schubkegel <rylanschubkegel@gmail.com>
2024-11-07 20:36:16 -0800

    Hopefully fix generate error

--- components/content/ContentImg.vue
+++ components/content/ContentImg.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-  import type { ResolvedImage } from '@nuxt/image'
-
   const props = defineProps<{ src: string; alt?: string; }>()
   const img = useImage()
   const format = computed(() => props.src.endsWith('.gif') ? 'gif' : 'webp')
-  const data = await img.getMeta(props.src)
-  const placeholder = img(props.src, {
-    height: 10,
-    format: format.value,
+  const placeholder = /https?:\/\//.test(props.src) ? undefined : img(props.src, {
+    height: 100,
+    format: 'webp',
     blur: 1,
     quality: 10
   })
@@ -15,13 +12,18 @@
 
 <template>
   <nuxt-img
+    v-if="placeholder"
     v-bind="$attrs"
-    :src="src"
+    :src="props.src"
     :alt="alt"
     :format="format"
     :placeholder="placeholder"
-    :width="data.width"
-    :height="data.height"
     loading="lazy"
   />
+  <img
+    v-else
+    v-bind="$attrs"
+    :src="props.src"
+    alt=""
+  >
 </template>
